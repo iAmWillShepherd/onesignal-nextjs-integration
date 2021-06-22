041d36c0f39d00dfcfbfbb4a10e2d6c2936fb532
William Shepherd <iAmWillShepherd@users.noreply.github.com>
2021-06-22 14:36:46 -0500

    Add web prompt

--- utils/useOneSignal.js
+++ utils/useOneSignal.js
@@ -3,13 +3,34 @@ import { useEffect } from "react";
 const useOneSignal = () =>
   useEffect(() => {
     window.OneSignal = window.OneSignal || [];
-    OneSignal.push(function () {
-      OneSignal.init({
-        appId: "Your App ID",
+
+    const { push, init, isPushNotificationsEnabled } = OneSignal;
+
+    push(() => {
+      init({
+        appId: "b40b7cc7-13dc-4662-8b48-efa668f9b72a",
         notifyButton: {
           enable: true,
         },
         allowLocalhostAsSecureOrigin: true,
+        promptOptions: {
+          slidedown: {
+            prompts: [
+              {
+                type: "sms",
+                autoPrompt: true,
+                text: {
+                  actionMessage: "Would you like to receive text messages?",
+                  acceptButton: "Sure",
+                  cancelButton: "Cancel",
+                },
+                delay: {
+                  timeDelay: 5,
+                },
+              },
+            ],
+          },
+        },
       });
     });
 
