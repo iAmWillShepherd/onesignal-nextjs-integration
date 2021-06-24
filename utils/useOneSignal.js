568627d1276ddc018466fbc472356692b6d2a66b
William Shepherd <iAmWillShepherd@users.noreply.github.com>
2021-06-24 10:11:18 -0500

    🧹

--- utils/useOneSignal.js
+++ utils/useOneSignal.js
@@ -1,11 +1,10 @@
-import { useEffect } from "react";
+import { useEffect } from "react"
 
 const useOneSignal = () =>
   useEffect(() => {
-    window.OneSignal = window.OneSignal || [];
+    window.OneSignal = window.OneSignal || []
 
-    const { push, init, isPushNotificationsEnabled, sendTag, setSMSNumber } =
-      OneSignal;
+    const { push, init, isPushNotificationsEnabled, sendTag } = OneSignal
 
     push(() => {
       init({
@@ -34,24 +33,22 @@ const useOneSignal = () =>
             ],
           },
         },
-      });
+      })
 
       // https://documentation.onesignal.com/docs/web-push-sdk#ispushnotificationsenabled
       isPushNotificationsEnabled(async (isEnabled) => {
         if (!isEnabled) {
-          console.log("Push notifications not enabled, sending tag...");
-
-          // https://documentation.onesignal.com/docs/sms-quickstart#setsmsnumber
+          console.log("Tagging user as not subscribed to push notifications.")
 
           // https://documentation.onesignal.com/docs/web-push-sdk#sendtag
-          const result = await sendTag("subscribed_to_push", "false");
+          const result = await sendTag("subscribed_to_push", "false")
         }
-      });
-    });
+      })
+    })
 
     return () => {
-      window.OneSignal = undefined;
-    };
-  }, []); // <-- run this effect once on mount
+      window.OneSignal = undefined
+    }
+  }, []) // <-- run this effect once on mount
 
-export default useOneSignal;
+export default useOneSignal
