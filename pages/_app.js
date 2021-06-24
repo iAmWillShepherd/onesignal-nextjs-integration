31f4cb04d21aa9a705e62611d36ad4b97215abe4
William Shepherd <iAmWillShepherd@users.noreply.github.com>
2021-06-24 10:10:53 -0500

    Move OneSignal setup to root component
    The entire project is for OneSignal, putting this here makes it available on all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,27 @@
+import Head from "next/head"
 import "../styles/globals.css"
 import useOneSignal from "../utils/useOneSignal"
 
 function MyApp({ Component, pageProps }) {
   useOneSignal()
-  return <Component {...pageProps} />
+
+  return (
+    <>
+      <Head>
+        <title>OneSignal + Next.js</title>
+        <meta
+          name="description"
+          content="Integrating OneSignal with a Next.js app."
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <script
+          src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
+          async=""
+        ></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import Head from "next/head"
-import Image from "next/image"
 import { useEffect } from "react"
 import styles from "../styles/Home.module.css"
 
@@ -11,35 +9,5 @@ export default function Home() {
     })
   }, [])
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>OneSignal + Next.js</title>
-        <meta
-          name="description"
-          content="Integrating OneSignal with a Next.js app."
-        />
-        <link rel="icon" href="/favicon.ico" />
-        <script
-          src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
-          async=""
-        ></script>
-      </Head>
-
-      <main className={styles.main}></main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  )
+  return <main className={styles.main}>Hello</main>
 }
